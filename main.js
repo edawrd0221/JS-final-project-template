fdf32445f1c62260d19e5872722e91ff38a27a63
edawrd0221 <edward2005.chiu@gmail.com>
2017-04-08 15:29:12 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 //設定畫布環境
  var bgImg = document.createElement("img");
  bgImg.src="images/map.png"
- var canvas =document.getElementById("game-canvas");
+ 
+var enemyImg = document.createElement("img");
+ enemyImg.src="images/jason.gif"
+ 
+
+
+
+
+
+
+var canvas =document.getElementById("game-canvas");
 var ctx =canvas.getContext("2d");
 
 //迪迪
 var enemy ={
   x:96,
   y:448,
-}
+};
 function draw(){
    ctx.drawImage(bgImg,0,0)
+  ctx.drawImage(enemyImg,enemy.x,enemy.y)
 }
-setTimeout(draw,1000);
+
+
+
+setInterval(draw,16);
