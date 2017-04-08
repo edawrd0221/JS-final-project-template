16421a83d35565ce47523981cf1315cc1c2ad82a
edawrd0221 <edward2005.chiu@gmail.com>
2017-04-08 14:48:33 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 //設定畫布環境
  var bgImg = document.createElament("img");
  bgImg.src="images/map.png"
- var canvas =document.getElementByID
+ var canvas =document.getElementByID("game-canvas");
+var ctx =canvas.getContext("2d");
