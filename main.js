c63ee552527add161447fdb4a2c5d12f584d30f2
edawrd0221 <edward2005.chiu@gmail.com>
2017-04-15 14:55:07 +0800

    Update main.js

--- main.js
+++ main.js
@@ -7,14 +7,25 @@ var enemyImg = document.createElement("img");
  
 var button = document.createElement("img");
  button.src="images/tower-btn.png"
+var lol= document.createElement("img");
+ button.src="images/tower.png"
 
 
+var cursur ={
+  x:0,
+ y:0
+}
 
+$("#game-canvas").on("mousemove",function(event)){
+                    cursur.y=event.offsetY
+                    cursur.x=event.offsetX
+})
 
 
 var canvas =document.getElementById("game-canvas");
 var ctx =canvas.getContext("2d");
-
+event.offsetX
+event.offsetY
 //迪迪
 var enemy ={
   x:96,
@@ -24,6 +35,8 @@ function draw(){
    ctx.drawImage(bgImg,0,0) 
   ctx.drawImage(enemyImg,enemy.x,enemy.y)
 ctx.drawImage(button,560,432,48,48)
+ctx.drawImage(lol,cursur.x,cursur.y)
+
 }
 
 
