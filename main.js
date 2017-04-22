a047325c01a516d8a0705bea6286208fa3ab9642
edawrd0221 <edward2005.chiu@gmail.com>
2017-04-22 14:16:56 +0800

    Update main.js

--- main.js
+++ main.js
@@ -55,7 +55,7 @@ function draw(){
    if(isBuild){
       ctx.drawImage(towerImg,cursor.x,cursor.y)
    }
-    ctx.drawImage(towerImg,towerImg.x,towerImg.y)
+    ctx.drawImage(towerImg,tower.x,tower.y)
 }
 
 
