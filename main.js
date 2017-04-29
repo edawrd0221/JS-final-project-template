86d4c8f9d6233b457a757722ab9296373030c640
edawrd0221 <edward2005.chiu@gmail.com>
2017-04-29 15:10:00 +0800

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ var enemy ={
    y:480-32,
    speedX:0,
    speedY:-64,
-   move:function(){
+   pathDes:0,
  move: function(){
   if(isCollided(enemyPath[this.pathDes].x,[this.pathDes].y,this.x,this.y,64/FPS,64/FPS)){
   }
