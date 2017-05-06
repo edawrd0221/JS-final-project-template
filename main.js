3dbdfd7726665ee5e9682c490c4c1dd698f1cae5
edawrd0221 <edward2005.chiu@gmail.com>
2017-05-06 14:17:22 +0800

    Update main.js

--- main.js
+++ main.js
@@ -24,8 +24,8 @@ var enemy ={
    speedY:-64,
    pathDes:0,
  move: function(){
-  console.log(isCollided(enemypath[this.pathDes].x,[this.pathDes].y,this.x,this.y,64/FPS,64/FPS));
-  if(isCollided(enemypath[this.pathDes].x,[this.pathDes].y,this.x,this.y,64/FPS,64/FPS)){
+ 
+  if(isCollided(enemypath[this.pathDes].x,enemypath[this.pathDes].y,this.x,this.y,64/FPS,64/FPS)){
    this.x =enemypath[ this.pathDes].x;
    this.y =enemypath[this.pathDes].y;
    this.pathDes=this.pathDes+1;
